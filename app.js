e139f458afe5cfc84d651bcb8be7ab05b710db9e
dennisnyarko <dennis_nyarko@outlook.com>
2022-09-29 23:48:31 +0000

    implement delete option

--- app.js
+++ app.js
@@ -70,6 +70,10 @@ app.post("/", function(req, res){
   
 });
 
+app.post("/delete", function(req, res){
+  console.log(req.body);
+});
+
 app.get("/work", function(req,res){
   res.render("list", {listTitle: "Work List", newListItems: workItems});
 });
