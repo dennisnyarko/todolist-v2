4d0860f41cc9a0cb7ace5ea24cb758881306c1df
dennisnyarko <dennis_nyarko@outlook.com>
2022-09-29 23:55:11 +0000

    show value when clicked

--- app.js
+++ app.js
@@ -71,7 +71,7 @@ app.post("/", function(req, res){
 });
 
 app.post("/delete", function(req, res){
-  console.log(req.body);
+  console.log(req.body.checkbox);
 });
 
 app.get("/work", function(req,res){
