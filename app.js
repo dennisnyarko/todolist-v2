7895b451b67f585448be9e0e14737c96d991afdd
dennisnyarko <dennis_nyarko@outlook.com>
2022-10-02 23:34:09 +0000

    connect to mongodb atlas

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true});
+mongoose.connect("mongodb+srv://cluster0.lttzarc.mongodb.net/myFirstDatabase" --username admin-dennis/todolistDB", {useNewUrlParser: true});
 
 const itemsSchema = {
   name: String
