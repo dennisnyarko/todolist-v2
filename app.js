db72de6894efb0ad16bf9816756ff9a685a16920
dennisnyarko <dennis_nyarko@outlook.com>
2022-10-04 09:45:04 +0000

    edit port connection

--- app.js
+++ app.js
@@ -140,8 +140,8 @@ let port = process.env.PORT;
 if (port == null || port == ""){
   port = 3000;
 }
-app.listen(port);
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+
+app.listen(port, function() {
+  console.log("Server has started successfully");
 });
